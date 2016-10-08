7311ac1583a5881cd95a1d1d163bd74c2e62321d
Jim Bracewell <jmsbrcwll@gmail.com>
2016-10-08 11:52:41 +0000

    initial commit

---
+++ dash.js
@@ -0,0 +1,7 @@
+var dash_button = require('node-dash-button');
+var dash = dash_button("50:f5:da:af:12:e8"); //address from step above
+dash.on("detected", function (){
+    console.log("omg found");
+});
+
+console.log('APP IS NOW RUNNING');
