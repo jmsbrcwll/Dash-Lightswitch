552a470e5f988136f7987c4910a7443d5aed390d
jmsbrcwll <jmsbrcwll@gmail.com>
2016-10-08 19:10:46 +0100

    Turns light on when dash button press is detected

--- dash.js
+++ dash.js
@@ -1,7 +1,17 @@
-var dash_button = require('node-dash-button');
+var dash_button = require('node-dash-button')
+var exec = require('child_process').exec, child;;
 var dash = dash_button("50:f5:da:af:12:e8", null, null, 'all'); //address from step above
 dash.on("detected", function (){
     console.log("omg found");
+
+	child = exec('python3 toggle.py username password bool,
+		function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error !== null) {
+				 console.log('exec error: ' + error);
+			}
+		});
 });
 
 console.log('APP IS NOW RUNNING');
