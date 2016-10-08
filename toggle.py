49dc2497e9beb16dd94968b7895a85ab20714e21
jmsbrcwll <jmsbrcwll@gmail.com>
2016-10-08 14:44:17 +0100

    Finished working toggle script

--- toggle.py
+++ toggle.py
@@ -13,9 +13,31 @@ def login(username, password):
 	return token
 
 def getDevices(token):
-	print('blah')	
+	print('attempting to get list of devices...')
+	resp = requests.get('https://wifiplugapi.co.uk:3081/zcloud/api/device_query?token=%s' % token)
+	deviceIds = []
+	for device in resp.json()['deviceList']:
+		id = device['id']
+		print('found device - id=%s' % id)
+		deviceIds.append(id)
+	
+	return deviceIds
+
+def deviceControl(token, device, status):
+	print('turning the device %s' % status)
+	payload = {
+		'token': token,
+		'device_id': device,
+		'on': status,
+	}
+	resp = requests.post('https://wifiplugapi.co.uk:3081/zcloud/api/device_control', data=payload)
+	print(resp.json())
 
 if __name__ == "__main__":
 	username = sys.argv[1]
 	password = sys.argv[2]
+	status = sys.argv[3]
 	token = login(username, password)
+	devices = getDevices(token)
+	for device in devices:
+		deviceControl(token, device, status)
