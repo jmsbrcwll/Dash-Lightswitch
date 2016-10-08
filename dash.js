f38ce132eb3f49f8788364393f30f7382fac99d0
jmsbrcwll <jmsbrcwll@gmail.com>
2016-10-08 13:07:27 +0100

    Fixed by making it listen for UDP as well as ARP

--- dash.js
+++ dash.js
@@ -1,5 +1,5 @@
 var dash_button = require('node-dash-button');
-var dash = dash_button("50:f5:da:af:12:e8"); //address from step above
+var dash = dash_button("50:f5:da:af:12:e8", null, null, 'all'); //address from step above
 dash.on("detected", function (){
     console.log("omg found");
 });
